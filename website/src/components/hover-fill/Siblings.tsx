51939257d7c5b7716a39d362c8de21cf10898bf6
yijiejiang <yijiejiang@tencent.com>
2025-04-28 12:13:56 +0800

    docs: chore

--- website/src/components/hover-fill/Siblings.tsx
+++ website/src/components/hover-fill/Siblings.tsx
@@ -8,6 +8,7 @@ import CodeBlock from '../CodeBlock';
 const Basics = () => {
   return (
     <div>
+      <h3 className="text-[17px] mt-6 font-semibold mb-3 inline-block group relative w-fit">As Light Buttons:</h3>
       <CodeBox className="gap-2 flex-wrap">
         <div>
           <Springen.HoverFill bgClassName="rounded-md">
@@ -57,14 +58,7 @@ const Basics = () => {
           </div>
         </Springen.HoverFill>
       </CodeBox>
-      <CodeBlock initialHeight={270}>{`import Springen from 'springen';
-
-<div>
-  <Springen.HoverFill className="flex-1">light-button</Springen.HoverFill>
-</div>
-<Springen.HoverFill className="flex-1">light-button</Springen.HoverFill>
-<Springen.HoverFill className="flex-1">light-button</Springen.HoverFill>`}</CodeBlock>
-      <hr className="my-4 border-t border-dashed border-neutral-200" />
+      <h3 className="text-[17px] mt-6 font-semibold mb-3 inline-block group relative w-fit">As Light Cards:</h3>
       <CodeBox className="mt-2 p-0">
         <div className="bg-white w-full">
           <svg className="block w-full" viewBox="0 0 793 68" fill="none">
@@ -117,14 +111,9 @@ const Basics = () => {
           </div>
         </div>
       </CodeBox>
-      <CodeBlock initialHeight={270}>{`import Springen from 'springen';
-
-{Array.from({ length: 6 }).map((_, index) => (
-  <Springen.HoverFill key={index} className="flex-1" bgClassName="rounded-lg">
-    <div className="flex p-4 gap-3" />
-  </Springen.HoverFill>
-))}`}</CodeBlock>
-      <hr className="my-4 border-t border-dashed border-neutral-200" />
+      <h3 className="text-[17px] mt-6 font-semibold mb-3 inline-block group relative w-fit">
+        As Light Form Controls in Table:
+      </h3>
       <CodeBox className="mt-2 p-0">
         <div className="bg-white w-full">
           <div className="flex items-center justify-between pl-6 pr-4 h-[68px] border-b border-neutral-100">
@@ -591,7 +580,7 @@ const Basics = () => {
           </div>
         </div>
       </CodeBox>
-      <hr className="my-4 border-t border-dashed border-neutral-200" />
+      <h3 className="text-[17px] mt-6 font-semibold mb-3 inline-block group relative w-fit">As Table Rows:</h3>
       <CodeBox className="mt-2 p-0">
         <div className="bg-white flex items-center justify-center p-5 w-full">
           <div>
@@ -677,6 +666,54 @@ const Basics = () => {
                 <div className="flex justify-end items-center pr-2 w-[77px]">低于50%</div>
               </div>
             </Springen.HoverFill>
+          </div>
+        </div>
+      </CodeBox>
+      <h3 className="text-[17px] mt-6 font-semibold mb-3 inline-block group relative w-fit">As List Items:</h3>
+      <CodeBox className="mt-2 p-0">
+        <div className="w-[366px] max-w-[95%] bg-white">
+          <div className="flex items-center justify-between h-[62px]">
+            <div className="flex items-center px-5">
+              <Springen.HoverFill bgClassName="rounded-sm">
+                <div className="flex items-center justify-center size-[30px]">
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M7.38564 6.00727C7.09274 5.71437 6.61787 5.71437 6.32498 6.00727L6.00678 6.32547C5.71389 6.61836 5.71389 7.09323 6.00678 7.38613L10.6207 12L6.00678 16.6139C5.71389 16.9068 5.71389 17.3816 6.00678 17.6745L6.32498 17.9927C6.61787 18.2856 7.09274 18.2856 7.38564 17.9927L11.9995 13.3789L16.6134 17.9927C16.9063 18.2856 17.3811 18.2856 17.674 17.9927L17.9922 17.6745C18.2851 17.3816 18.2851 16.9068 17.9922 16.6139L13.3784 12L17.9922 7.38613C18.2851 7.09323 18.2851 6.61836 17.9922 6.32547L17.674 6.00727C17.3811 5.71437 16.9063 5.71437 16.6134 6.00727L11.9995 10.6211L7.38564 6.00727Z"
+                      fill="#464749"
+                    />
+                  </svg>
+                </div>
+              </Springen.HoverFill>
+              <i className="ml-3 mr-4 w-px h-[26px] bg-neutral-200" />
+              <div className="text-[18px] font-semibold">自定义列表</div>
+            </div>
+            <Springen.HoverFill bgClassName="rounded-sm">
+              <div className="flex items-center justify-center w-[46px] h-[30px] font-medium text-[13px] text-neutral-500">
+                重置
+              </div>
+            </Springen.HoverFill>
+          </div>
+          <div className="flex items-center pl-6 h-[54px] text-sm bg-neutral-100 border-y border-neutral-200">
+            默认列：创作者、亮点视频、亮点标签
+          </div>
+          <div className="p-3">
+            {['粉丝量级', '粉丝增长量', '粉丝增长率', '平均播放量', '播放中位数'].map((item) => (
+              <Springen.HoverFill bgClassName="rounded-lg" key={item}>
+                <div className="flex items-center justify-between pl-3 pr-2 h-12 text-sm">
+                  {item}
+                  <div className="flex items-center justify-center size-8 rounded-sm hover:bg-neutral-200 transition-all">
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M16.0004 20.4446C13.4615 20.4446 11.1557 19.0515 9.08296 16.2654C8.96584 16.108 8.96584 15.8923 9.08296 15.7349C11.1557 12.9488 13.4615 11.5557 16.0004 11.5557C18.5393 11.5557 20.8451 12.9487 22.9179 15.7348C23.035 15.8922 23.035 16.1079 22.9179 16.2653C20.8451 19.0514 18.5393 20.4446 16.0004 20.4446ZM16.0004 19.1112C17.7186 19.1112 19.1115 17.7183 19.1115 16.0001C19.1115 14.2819 17.7186 12.889 16.0004 12.889C14.2822 12.889 12.8893 14.2819 12.8893 16.0001C12.8893 17.7183 14.2822 19.1112 16.0004 19.1112ZM16.0004 17.7779C15.0186 17.7779 14.2226 16.9819 14.2226 16.0001C14.2226 15.0183 15.0186 14.2223 16.0004 14.2223C16.9823 14.2223 17.7782 15.0183 17.7782 16.0001C17.7782 16.9819 16.9823 17.7779 16.0004 17.7779Z"
+                        fill="#296BEF"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </Springen.HoverFill>
+            ))}
           </div>
         </div>
       </CodeBox>
